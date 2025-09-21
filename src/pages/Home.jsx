01a2150634b78879553f2143d70f401bf5d4f22f
CuevasErik-dev <cuevas.erik.israel@gmail.com>
2025-09-21 16:00:21 -0600

    task #8 Ajustando Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,38 @@
 
-import { Card, PaperProvider } from 'react-native-paper';
+import { Card,Text, PaperProvider, Avatar } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet } from 'react-native';
+
+/* importacion de componentes */
 import CardTitle from '../components/Card/CardTitle';
 
 
 const Home = () => (
     <PaperProvider>
-        <SafeAreaView>
+        <SafeAreaView style={style.container}>
             <CardTitle/>
+            <Card.Content style={style.Content}>
+                <Text variant='headlineLarge' style={style.texto}> Bienvenido </Text>
+                <Avatar.Image size={200} 
+                source={{uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgpnmY-O9iz09Jka-vGvK2Lv-U-pL3H18CfA&s"}}/>
+            </Card.Content>
         </SafeAreaView>
     </PaperProvider>    
 
 );
 
 export default Home;
+
+const style = StyleSheet.create({
+    container:{
+        flex:1
+    },
+    Content:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    texto:{
+        paddingBottom:10
+    }
+});
