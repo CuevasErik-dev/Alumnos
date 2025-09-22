4a054bdb620de4c912c48c38dbdc31806137a273
CuevasErik-dev <cuevas.erik.israel@gmail.com>
2025-09-21 20:14:35 -0600

    task #13 Haciendo uso de useTheme

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StyleSheet, Text, View } from "react-native";
-import { PaperProvider } from "react-native-paper";
+import { StyleSheet, Text, View, Platform } from "react-native";
+import { StatusBar } from "expo-status-bar"
+import { PaperProvider, MD3LightTheme } from "react-native-paper";
 import NavegationPrincipal from "./src/components/Navigation/NavegationPrincipal";
 
+const theme = {
+  ...MD3LightTheme,
+  colors: {
+    ...MD3LightTheme.colors,
+    primary: "#ee15dcff",      // color principal de botones y textos destacados
+    secondary: "#03dac6",    // color secundario
+    background: "#6b6a6aff",   // fondo general de la app
+    surface: "#ffffff",       // fondo de tarjetas y paneles
+    accent: "#03dac6",
+    error: "#B00020",
+  },
+};
 export default function App() {
+
   return (
-    <PaperProvider>
-      <NavegationPrincipal/>
+    <PaperProvider theme={theme}>
+      <NavegationPrincipal />
+      <StatusBar style="light" backgroundColor={theme.colors.background} />
     </PaperProvider>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
